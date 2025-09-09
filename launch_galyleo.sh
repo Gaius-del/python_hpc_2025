03a688d164a476585eeb610add431acb3a50fcac
Andrea Zonca <code@andreazonca.com>
2025-09-09 09:14:19 -0700

    remove res and add account variable

--- launch_galyleo.sh
+++ launch_galyleo.sh
@@ -1,19 +1,16 @@
 # Use current folder
 NOTEBOOK_FOLDER=$(pwd -P)
-## Create link
-#cd 1_dask_tutorial
-#rm -f data
-#ln -s /expanse/lustre/projects/sds166/zonca/dask_tutorial_data data
-#cd ../
+export PATH="/cm/shared/apps/sdsc/galyleo:${PATH}"
+# specify your own allocation here
+ACCOUNT=sds166
 
 galyleo launch \
-  --account ${SI25_ACCOUNT} \
+  --account ${ACCOUNT} \
   --partition compute \
   --cpus 128 \
   --memory 242 \
   --time-limit 04:00:00 \
   --conda-yml environment.yaml \
   --notebook-dir ${NOTEBOOK_FOLDER} \
-  --reservation ${SI25_RES_CPU} \
   --cache \
   --quiet
